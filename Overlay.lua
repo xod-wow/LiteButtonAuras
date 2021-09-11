e7ef904eed5835aa439560188ebcabbabac1db6e
Mike "Xodiv" Battersby <mib@post.com>
2021-09-11 10:27:01 +1000

    Working duration display

--- Overlay.lua
+++ Overlay.lua
@@ -13,6 +13,7 @@ local _, LBA = ...
 LiteButtonAurasOverlayMixin = {}
 
 function LiteButtonAurasOverlayMixin:OnLoad()
+    self.Glow:SetAlpha(0.6)
 end
 
 function LiteButtonAurasOverlayMixin:ScanAction()
@@ -30,15 +31,51 @@ function LiteButtonAurasOverlayMixin:ScanAction()
         elseif spellID then
             self.name = GetSpellInfo(spellID)
         end
+    else
+        self.name = nil
     end
 end
 
+function LiteButtonAurasOverlayMixin:OnUpdate()
+    self:UpdateDuration()
+end
+
+local function DurationAbbrev(duration)
+    if duration >= 86400 then
+        return "%dd", math.ceil(duration/86400)
+    elseif duration >= 3600 then
+        return "%dh", math.ceil(duration/3600)
+    elseif duration >= 60 then
+        return "%dm", math.ceil(duration/60)
+    elseif duration >= 5 then
+        return "%d", duration
+    else
+        return "%0.1f", duration
+    end
+end
+
+function LiteButtonAurasOverlayMixin:UpdateDuration()
+    if self.expireTime then
+        local duration = self.expireTime - GetTime()
+        self.Duration:SetFormattedText(DurationAbbrev(duration))
+        self.Duration:Show()
+    else
+        self.Duration:Hide()
+    end
+end
+
+-- name, icon, count, debuffType, duration, expirationTime, source, isStealable, nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod, ...
+
 function LiteButtonAurasOverlayMixin:ShowBuff(index)
+    self.expireTime = select(6, UnitAura('player', index))
     self.Glow:SetVertexColor(0.7, 1.0, 0.7)
     self:Show()
 end
 
 function LiteButtonAurasOverlayMixin:ShowDebuff(index)
+    self.expireTime = select(6, UnitAura('target', index))
+    self.Glow:SetVertexColor(1.0, 0.7, 0.7)
+    self:Show()
 end
 
 function LiteButtonAurasOverlayMixin:ShowInterrupt(index)
